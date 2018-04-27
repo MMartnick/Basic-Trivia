71baf2c039e0d9daa4b6415ad024855cd2368f17
Martnick1 <Martnickmatthew@gmail.com>
2018-04-27 00:36:51 -0500

    Last update for tonight. cleaned up design, tried to implement counter and reset

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,8 +1,7 @@
-
 // Array for questions
 var triviaQuest = [
 
-		{
+	{
 		question: "What do the Misfits want?",
 		answers: {
 			a: "Your heart",
@@ -93,81 +92,80 @@ var triviaQuest = [
 ];
 
 
-
+//global variables
 var triviaContainer = document.getElementById('quiz');
 var resultsContainer = document.getElementById('results');
 var submit = document.getElementById('submit');
 var start = document.getElementById('start');
+var reset = document.getElementById('reset').style.display = "none";
 var logo = document.getElementById('logo');
-var hide = submit.style.display==="none";
+var hide = submit.style.display === "none";
 var clock;
 var timeLeft = 30;
 var countdownClock = $("#timer");
 
-generateQuiz(triviaQuest, triviaContainer, resultsContainer, submit, start);
+generateQuiz(triviaQuest, triviaContainer, resultsContainer, submit, start, reset);
 
-function generateQuiz(questions, triviaContainer, resultsContainer, submit, start){
+function generateQuiz(questions, triviaContainer, resultsContainer, submit, start, reset) {
 
-	function showQuestions(questions, triviaContainer){
-		// we'll need a place to store the output and the answer choices
+	function showQuestions(questions, triviaContainer) {
 		var output = [];
 		var answers;
 
-		// for each question...
-		for(var i = 0; i < questions.length; i++){
-			
+		for (var i = 0; i < questions.length; i++) {
+
 			// reset answer
 			answers = [];
 
-			// add buttons to for each answer
-			for(letter in questions[i].answers){
+			// add buttons for each answer
+			for (letter in questions[i].answers) {
 				answers.push(
-					'<label>'
-						+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
-						+ letter + ': '
-						+ questions[i].answers[letter]
-					+ '</label>'
+					'<label>' +
+					'<input type="radio" name="question' + i + '" value="' + letter + '">' +
+					letter + ': ' +
+					questions[i].answers[letter] +
+					'</label>'
 				);
 			}
 
 			// add this question and its answers to the output
 			output.push(
-				'<div class="question">' + questions[i].question + '</div>'
-				+ '<div class="answers">' + answers.join('') + '</div>'
+				'<div class="question">' + questions[i].question + '</div>' +
+				'<div class="answers">' + answers.join('') + '</div>'
 			);
 		}
 
 		// Use output to show questions
 		triviaContainer.innerHTML = output.join('');
-		
+
 	}
 
 
-	function showResults(questions, triviaContainer, resultsContainer){
-		
-		// gather answer containers from our quiz
+	function showResults(questions, triviaContainer, resultsContainer) {
+
+		// coolects answers from our quiz
 		var answerContainers = triviaContainer.querySelectorAll('.answers');
-		
+
 		// keep track of user's answers
 		var userAnswer = '';
 		var numCorrect = 0;
-		
-		// for each question...
-		for(var i=0; i<questions.length; i++){
 
-			// find selected answer
-			userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-			
+	
+		for (var i = 0; i < questions.length; i++) {
+
+			// finds selected answer
+			userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
+
 			// if answer is correct
-			if(userAnswer===questions[i].rightAnswer){
+			if (userAnswer === questions[i].rightAnswer) {
 				// add to the number of correct answers
 				numCorrect++;
-				
+
 				// change to green
 				answerContainers[i].style.color = 'lightgreen';
 			}
 			// if answer is wrong or blank
-			else{
+			else {
 				// change to red
 				answerContainers[i].style.color = 'red';
 			}
@@ -178,21 +176,52 @@ function generateQuiz(questions, triviaContainer, resultsContainer, submit, star
 	}
 
 	// show questions on start
-	start.onclick = function(){
-		showQuestions(questions, triviaContainer, submit);
-		// set timelimit from start
-		setTimeout(thirtySeconds, 1000 * 50);
-		function thirtySeconds() {
-			$("#quiz").hide();
-			document.getElementById("#quiz").style.display = "none";
+	start.onclick = function () {
+		showQuestions(questions, triviaContainer, submit, reset);
+		$("#start").hide();
+		$("#reset").show();
+
+		//set up for timer
+		$("#start").on("click", run);
+
+		function run() {
+			clearInterval(intervalId);
+			intervalId = setInterval(decrement, 1000 * 50);
 		}
+
+		function decrement() {
+			number--;
+			$("#timer").html("<h2>" + number + "</h2>");
+			if (number === 0) {
+				stop();
+				alert("Time Up!");
+			}
+		}
+		clearInterval(intervalId);
+
+		run();
+
 	}
-	
+
+	// set time limit from start
+	setTimeout(fiftySeconds, 1000 * 50);
+
+	function fiftySeconds() {
+		$("#quiz").hide();
+		document.getElementById("#quiz").style.display = "none";
+	}
+
+
+
+
 	// show results on click
-	submit.onclick = function(){
+	submit.onclick = function () {
 		showResults(questions, triviaContainer, resultsContainer);
 	}
 
 
-	
-}	
+	//resets page
+	start.reset()
+
+
+}
